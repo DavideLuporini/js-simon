6b5b2a38f2f5508a61e0dc0a6697e445aafef2fa
illupovero <davideluporini1996@gmail.com>
2022-01-24 16:02:34 +0100

    added random number + alert

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ console.log('js ok')
 // Bonus:
 // controllare che i numeri casuali siano diversi tra loro
 // controllare che l'utente non inserisca 2 volte lo stesso numero
+
+// funzione per generare numeri random
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// dichiaro 5 numeri casuali che l'utente dovrà memorizzare e poi inserire
+let number1 = getRandomNumber(1, 100);
+let number2 = getRandomNumber(1, 100);
+let number3 = getRandomNumber(1, 100);
+let number4 = getRandomNumber(1, 100);
+let number5 = getRandomNumber(1, 100);
+
+
+// dichiaro un array dove comprendo i 5 numeri che l'utente dovrà memorizzare e poi reinserire
+const listNumber = [number1, number2, number3, number4, number5]
+console.log(listNumber)
+
+
+// inserisco i numeri in un alert 
+
+alert(`questi sono i numeri che dovrai memorizzare: 
+
+${listNumber}
+
+Ricorda: Quando premerai ok , dopo 30 secondi , dovrai inserire uno ad uno i numeri che avrai memorizzato
+allo scopo di indovinarne quanti più possibile`)
